be9f48e7218b4e2041c5fc22a0c5102e9f2dd170
yangmian1167 <41039196+yangmian1167@users.noreply.github.com>
2019-09-09 14:01:46 +0800

    Update newplay.lua

--- AOC/newplay.lua
+++ AOC/newplay.lua
@@ -38,21 +38,21 @@ function new_other()
 						else
 							if UI_pic('新手','战斗_选择2级兵',false)then
 								delay(5)
-								click(587,613,2) 	--点兵
+								click(587,613) 	--点兵
 								delay(2)
-								click(811,611,2) 	--点兵
+								click(811,611) 	--点兵
 								delay(2)
-								click(811,611,2) 	--点兵
+								click(811,611) 	--点兵
 								delay(2)
-								click(811,611,2) 	--点兵
+								click(811,611) 	--点兵
 								delay(2)
-								click(811,611,2) 	--点兵
+								click(811,611) 	--点兵
 								delay(2)
-								click(1086,663,2) 	--点英雄
+								click(1086,663) 	--点英雄
 								delay(2)
-								click(1086,663,2) 	--点英雄
+								click(1086,663) 	--点英雄
 								delay(2)
-								click(1086,663,2) 	--点英雄
+								click(1086,663) 	--点英雄
 							else
 								click(1086,663,0.2) 	--点英雄
 								log('点英雄')
