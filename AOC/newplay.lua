cdcbea32cdce19a9d96bd11d8a291c25d646eaf4
yangmian1167 <41039196+yangmian1167@users.noreply.github.com>
2019-09-10 14:31:06 +0800

    Update newplay.lua

--- AOC/newplay.lua
+++ AOC/newplay.lua
@@ -34,7 +34,7 @@ function new_other()
 							return '战斗失败'
 						end
 					else
-						if UI_pic('新手','战斗_选择2级兵',false)then
+						if UI_pic('新手','战斗_选择2级兵',true,1)then
 							log('休息5秒')
 							delay(5)
 							click(587,613) 	--点兵
