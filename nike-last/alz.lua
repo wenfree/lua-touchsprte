6cde7426a6470bfbdf5ef9f7d68ef81c61ef65dd
ouwen000 <wenhong520@qq.com>
2019-09-20 22:23:10 +0800

    Update alz.lua

--- nike-last/alz.lua
+++ nike-last/alz.lua
@@ -1,3 +1,5 @@
+
+
 function _vCode_gx(User,Pass,PID) --共享平台
     local token,uid,number = "",""
     return {
@@ -119,25 +121,29 @@ end
 
 --165
 function _vCode_dm()
-	local User = 'api-235018-rBGkUMQ'
+	local User = 'shuaishuai'
 	local Pass = 'shuai888'
-	local PID = '18312'
+	local PID = '3215'
     local token,uid,number = "",""
-	local api_url = "http://api.lianxins.com/api/do.php"
+	local api_url = "http://api.yyyzmpt.com/index.php/"
     return {
 	    login=(function() 
             local RetStr
 			for i=1,5,1 do
 				toast("获取token\n"..i.."次共5次")
                 mSleep(1500)
-                RetStr = httpGet(api_url..'?action=loginIn&name='..User..'&password='..Pass)
+				local urls = api_url..'reg/login?loginIn&username='..User..'&password='..Pass
+				nLog(urls)
+                RetStr = get(urls)
 				if RetStr then
-					nLog(RetStr)
-					RetStr = strSplit(RetStr,"|")
-					if RetStr[1] == 1 or RetStr[1] == '1' then
-						token = RetStr[2]
-						log('token='..token,true)
+					if RetStr['errno'] == 0 then
+						token = RetStr['ret']['token']
+--						log('token='..token,true)
+						nLog('token='..token)
 						break
+					else
+						toast(RetStr.errmsg)
+						mSleep(2000)
 					end
 				end
 			end
@@ -145,30 +151,43 @@ function _vCode_dm()
         end), 
 		getPhone=(function()
             local RetStr=""
-			RetStr = httpGet(api_url.."?action=getPhone&sid="..PID.."&token="..token)
-			if RetStr ~= "" and  RetStr ~= nil then
-				RetStr = strSplit(RetStr,"|")
+			local urls = api_url..'clients/online/setWork?loginIn&token='..token..'&t=1&pid='..PID
+			nLog(urls)
+			RetStr = get(urls)
+			if RetStr then
+				if RetStr['errno'] == 0 then
+					number = RetStr['ret']['number']
+--					log('token='..token,true)
+					nLog('number='..number)
+					return number
+				else
+					toast(RetStr.errmsg)
+					mSleep(2000)
+				end
 			end
-			if RetStr[1] == 1 or RetStr[1]== '1' then
-				number = RetStr[2]
-				return number
-			end
+
         end),
 	    getMessage=(function()
 			local Msg
             for i=1,25,1 do
                 mSleep(3000)
-                RetStr = httpGet(api_url.."?action=getMessage&sid="..PID.."&token="..token.."&phone="..number)
+				local urls = api_url..'clients/sms/getSms?loginIn&type=1&token='..token..'&project='..PID..'&number='..number
+				nLog(urls)
+				RetStr = get(urls)
 				if RetStr then
-					local arr = strSplit(RetStr,"|") 
-					if arr[1] == '1' then 
-						Msg = arr[2]
+					if RetStr['errno'] == 0 then
+						Msg = RetStr['ret']['tst']
+	--					log('token='..token,true)
+						nLog('Msg='..Msg)
 						local i,j = string.find(Msg,"%d+")
 						Msg = string.sub(Msg,i,j)
+						return Msg
+					else
+						toast(RetStr.errmsg)
+						mSleep(2000)
 					end
-					if type(tonumber(Msg))== "number" then return Msg end
 				end
-                toast(tostring(RetStr).."\n"..i.."次共25次")
+                toast(i.."次共25次")
             end
             return ""
         end),
