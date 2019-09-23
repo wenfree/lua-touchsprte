8cf535a5b26bf2fd4cce39c0adeb78b20ffab77d
ouwen000 <wenhong520@qq.com>
2019-09-23 14:36:08 +0800

    Update ui.lua

--- AOC/ui.lua
+++ AOC/ui.lua
@@ -505,7 +505,7 @@ function other()
 	elseif UI('other','国家发展宝箱-领取',true,1)then
 	elseif UI_pic('other','打开礼物',true)then
 	elseif UI('other','选择国家界面')then
-		if d('龙牙大陆选国界面') then
+		if UI('other','选择国家界面') then
 			龙牙国家选择 = tonumber(UIdata.world)
 			click(t['龙牙国家位置'][龙牙国家选择][1],aoc['龙牙国家位置'][龙牙国家选择][2])
 			UI('other','选国界面',true,1)
