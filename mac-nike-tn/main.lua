0c908e4e67e225e7766647b490a8e65155deab41
ouwen000 <wenhong520@qq.com>
2020-03-22 20:19:45 +0800

    铁牛

--- mac-nike-tn/main.lua
+++
@@ -1,6 +0,0 @@
--- mac-nike-tn
--- main.lua  
-
--- Create By TouchSpriteStudio on 02:20:26   
--- Copyright © TouchSpriteStudio . All rights reserved.
-	
--- 铁牛nike/main.lua
+++
@@ -1,9 +0,0 @@
--- nike
--- main.lua  
-
--- Create By TouchSpriteStudio on 00:05:42   
--- Copyright © TouchSpriteStudio . All rights reserved.
-	
-	
-nLog('main');
-nLog(appDataPath(frontAppBid()))
